ca90aae8b2b8a17ec433115e9bd3a551a05b17cd
Иван <imihajlow@gmail.com>
2014-03-07 09:27:04 +0400

    uart scratch

--- uart/uart_rx.v
+++ uart/uart_rx.v
@@ -41,6 +41,54 @@ module UartReceiver(
             end else begin
                 clkCounter <= clkCounter + 1;
             end
+            if(receiveData) begin
+                dataReceived <= 1'b0;
+                overflow <= 1'b0;
+                break <= 1'b0;
+            end
         end
+    end
+
+    wire [3:0] totalDataBits = 4'd5 + dataBits + hasParity - 1'd5;
+    reg [3:0] dataCounter = 4'd0;
+    reg [8:0] currentData = 9'd0;
+
+    reg firstStopBitReceived = 1'b0;
+    wire currentParityError = 1'b0;
+
+    always @(posedge uartClk) begin
+        case(state)
+            STATE_START:
+                if(rx == 1'b1)
+                    state <= STATE_IDLE;
+                else begin
+                    state <= STATE_DATA;
+                    dataCounter <= 0;
+                end
+            STATE_DATA:
+                currentData[dataCounter] <= rx;
+                if(dataCounter < totalDataBits) begin
+                    dataCounter <= dataCounter + 4'd1;
+                end else begin
+                    state <= STATE_STOP;
+                    firstStopBitReceived <= 1'b0;
+                end
+            STATE_STOP:
+                if(rx == 1'b1) begin
+                    if(!extraStopBit || firstStopBitReceived) begin
+                        state <= STATE_IDLE;
+                        if(dataReceived)
+                            overflow <= 1'b1;
+                        dataReceived <= 1'b1;
+                        dataOut <= currentData;
+                        parityError <= currentParityError;
+                        break <= 1'b0;
+                    end
+                    firstStopBitReceived <= 1'b1;
+                end else begin
+                    break <= 1'b1;
+                    state <= STATE_IDLE;
+                end
+        endcase
     end
 endmodule
