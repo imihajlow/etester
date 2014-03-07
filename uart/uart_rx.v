8e939597b567e4f2442170b2956b64b04c2b4851
imihajlov <imihajlow@gmail.com>
2014-03-07 13:20:40 +0400

    8n1 receive

--- uart/uart_rx.v
+++ uart/uart_rx.v
@@ -13,16 +13,34 @@ module UartReceiver(
     output reg parityError,
     output reg overflow,
     output reg break,
-    input receiveData;
+    input receiveData
 );
     parameter CLOCK_DIVISOR_WIDTH=24;
     localparam STATE_IDLE = 3'd0;
     localparam STATE_START = 3'd1;
     localparam STATE_DATA = 3'd2;
     localparam STATE_STOP = 3'd3;
-    reg [2:0] state;
-    reg [CLOCK_DIVISOR_WIDTH-1:0] clkCounter;
-    wire uartClk = clkCounter[CLOCK_DIVISOR_WIDTH-1];
+
+    reg [2:0] state = STATE_IDLE;
+
+    /* Slow clock begin */
+    reg [CLOCK_DIVISOR_WIDTH-1:0] clockCounter = 0;
+    wire uartClk = clockCounter > (clockDivisor >> 1);
+    always @(posedge clk) begin
+        if(rst) begin
+            clockCounter <= 0;
+        end else begin
+            if(state == STATE_IDLE) begin
+                clockCounter <= 0;
+            end else begin
+                if(clockCounter != clockDivisor)
+                    clockCounter <= clockCounter + 1;
+                else
+                    clockCounter <= 0;
+            end
+        end
+    end
+    /* Slow clock end */
 
     always @(posedge clk) begin
         if(rst) begin
@@ -31,15 +49,12 @@ module UartReceiver(
             parityError <= 1'b0;
             overflow <= 1'b0;
             break <= 1'b0;
-            clkCounter <= 0;
+            state <= STATE_IDLE;
         end else begin
             if(state == STATE_IDLE) begin
                 if(rx == 1'b0) begin
                     state <= STATE_START;
                 end
-                clkCounter <= 0;
-            end else begin
-                clkCounter <= clkCounter + 1;
             end
             if(receiveData) begin
                 dataReceived <= 1'b0;
@@ -49,7 +64,7 @@ module UartReceiver(
         end
     end
 
-    wire [3:0] totalDataBits = 4'd5 + dataBits + hasParity - 1'd5;
+    wire [3:0] totalDataBits = 4'd5 + dataBits + hasParity - 4'd1;
     reg [3:0] dataCounter = 4'd0;
     reg [8:0] currentData = 9'd0;
 
@@ -59,22 +74,24 @@ module UartReceiver(
     always @(posedge uartClk) begin
         case(state)
             STATE_START:
-                if(rx == 1'b1)
+                if(rx)
                     state <= STATE_IDLE;
                 else begin
                     state <= STATE_DATA;
                     dataCounter <= 0;
                 end
-            STATE_DATA:
-                currentData[dataCounter] <= rx;
-                if(dataCounter < totalDataBits) begin
-                    dataCounter <= dataCounter + 4'd1;
-                end else begin
-                    state <= STATE_STOP;
-                    firstStopBitReceived <= 1'b0;
+            STATE_DATA: begin
+                    currentData[dataCounter] <= rx;
+                    $display("@%t got bit %b", $time, rx);
+                    if(dataCounter < totalDataBits) begin
+                        dataCounter <= dataCounter + 4'd1;
+                    end else begin
+                        state <= STATE_STOP;
+                        firstStopBitReceived <= 1'b0;
+                    end
                 end
             STATE_STOP:
-                if(rx == 1'b1) begin
+                if(rx) begin
                     if(!extraStopBit || firstStopBitReceived) begin
                         state <= STATE_IDLE;
                         if(dataReceived)
