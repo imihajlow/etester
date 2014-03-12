87b14f7c0e909c864ad398553ae4d28c2fd95a8a
imihajlov <imihajlow@gmail.com>
2014-03-12 16:42:05 +0400

    modbus.v

---
+++ uart/modbus.v
@@ -0,0 +1,17 @@
+module ModbusSlave(
+    input clk,
+    input rst,
+
+    // Read Fifo
+    input empty,
+    output readReq,
+    input readAck,
+    input [7:0] dataOut,
+
+    // Write fifo
+    input full,
+    output writeReq,
+    input writeAck,
+    output [7:0] dataIn    
+);
+endmodule
